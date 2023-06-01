329edf72ff70efa04baae709e9cef09873564b2b
NaGeon87 <nageon87@gmail.com>
2023-06-01 10:04:46 +0900

    print_reviews

--- db/connection.py
+++ db/connection.py
@@ -8,13 +8,31 @@
 # 3.결과 받기        (Python <- DB)
 # 4.Connection 읽기 (Python X DB)
 
+# MongoDB 구조
+# - MongoDB(DBMS): 데이터베이스 관리 시스템
+#   - DB(NAVER): 프로젝트 단위
+#         - Collection(회원) - CRUD
+#         - Collection(카페) - CRUD
+#         - Collection(쇼핑) - CRUD
+#         - Collection(메일) - CRUD
+#   - DB(KAKAO)
+#   - DB(BLOG)
+
+# MongoDB 데이터 주고 받기
+# - MongoDB는 BSON Type으로 데이터를 주고 받음
+# - BSON(Binary JSON) = JSON과 거의 동일
+# - 그냥 JSON Type으로 사용하면 됨(문제 없음)
+# - Python에서 JSON은 Dict Type 사용!(Python Dict = JSON)
+
 from pymongo import MongoClient
 
 
 # MongoDB Connection
 def conn():
+    #           mongodb_web: ip + port + id%pw
+    # Python-mongodb 연결 => client
     client = MongoClient("mongodb+srv://cnu:cnu1234@movieg.xszibhw.mongodb.net/")  # IP, Port, ID&PW
-    db = client["movie"]
+    db = client["review"]  # DB 선택
     
-    collection = db.get_collection("movie")
+    collection = db.get_collection("movie")  # Collection 선택
     return collection
